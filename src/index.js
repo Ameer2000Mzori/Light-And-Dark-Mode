7556c5cf0ff44adf387578b2c94fcc507715abc4
Ameer <ameerapex496@gmail.com>
2023-11-07 03:07:56 +0100

    finished about me section with dynamic styling

--- src/index.js
+++ src/index.js
@@ -3,20 +3,39 @@ import menubar from "./asset/menubar.png";
 import closebar from "./asset/closebar.png";
 import lightLogo from "./asset/lightModelogo.svg";
 import darkLogo from "./asset/darkModelogo.svg";
+import darkBoard from "./asset/darkModeboard.svg";
+import lightBoard from "./asset/lightModeboard.svg";
+import darkStatus from "./asset/darkModestatus.svg";
+import lightStatus from "./asset/lightModestatus.svg";
+import darkWeb from "./asset/darkModeweb.svg";
+import lightWeb from "./asset/lightModeweb.svg";
 
 const btnWrap = document.getElementsByClassName("btn-wrap")[0];
 const navbrandimg = document.getElementsByClassName("nav-brand-img")[0];
-navbrandimg.src = darkLogo;
-navbrandimg.src = lightLogo;
+
 // light and dark mode
 const toggleSwitch = document.getElementById("box-check");
 const switcherWrapper = document.getElementsByClassName("switcher-wrapper")[0];
 const btnImg = document.getElementsByClassName("btn-img")[0];
 
+btnImg.src = menubar;
+
+const img1 = document.getElementsByClassName("img-1")[0];
+const img2 = document.getElementsByClassName("img-2")[0];
+const img3 = document.getElementsByClassName("img-3")[0];
+img1.src = lightBoard;
+img2.src = lightStatus;
+img3.src = lightWeb;
 function imgesChange(color) {
   if (color === "light") {
+    img1.src = lightBoard;
+    img2.src = lightStatus;
+    img3.src = lightWeb;
     navbrandimg.src = lightLogo;
   } else {
+    img1.src = darkStatus;
+    img2.src = darkBoard;
+    img3.src = darkWeb;
     navbrandimg.src = darkLogo;
   }
 }
@@ -49,6 +68,7 @@ toggleSwitch.addEventListener("change", (event) => {
 });
 
 // this is navbar
+navbrandimg.src = lightLogo;
 btnImg.src = menubar;
 const navLinks = document.getElementsByClassName("nav-links")[0];
 btnWrap.addEventListener("click", () => {
